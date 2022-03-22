2ce7e4b08d14c51e285ba7b1722ded4e7af5fc63
harshadalohar123 <harshalohar123@gmail.com>
2022-03-22 13:07:44 +0530

    ADDED:Max From 3 Float

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GenericProblem
+{
+    public class Uc2_Generic
+    {
+        public static float MaxFloatNo(float First_Value, float Second_Value, float Third_Value)
+        {
+            if (First_Value.CompareTo(Second_Value) > 0)
+            {
+                if (First_Value.CompareTo(Third_Value) > 0)
+                {
+                    return First_Value;
+                }
+                else
+                {
+                    return Third_Value;
+                }
+            }
+            else
+            {
+                if (Second_Value.CompareTo(Third_Value) > 0)
+                {
+                    return Second_Value;
+                }
+                else
+                {
+                    return Third_Value;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum Number");
+            float result = Uc2_Generic.MaxFloatNo(13.6F, 90.432F, 160.4F);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
