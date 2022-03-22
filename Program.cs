d7a1145167a1406a8d56c8ab83eb77d4c7e571a4
harshadalohar123 <harshalohar123@gmail.com>
2022-03-22 23:42:06 +0530

    ADDED: Find Max value Using Generic Class

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RefactorGeneric
+{
+    public class Refactor_2
+    {
+        static void Main(string[] args)
+        {
+            GenericMaximum<int> obj=new GenericMaximum<int>(23, 45, 10);
+            int result = obj.MaxMethod();
+            Console.WriteLine("Maximum value of integer is= " + result);
+
+            GenericMaximum<double> obj1 = new GenericMaximum<double>(45.9, 45.76, 96.99);
+            double result1 = obj1.MaxMethod();
+            Console.WriteLine("Maximum value of double is= " + result1);
+
+            GenericMaximum<string> obj2 = new GenericMaximum<string>("454", "687", "963");
+            string result2 = obj2.MaxMethod();
+            Console.WriteLine("Maximum value of string is= " + result2);
+
+        }
+    }
+}
