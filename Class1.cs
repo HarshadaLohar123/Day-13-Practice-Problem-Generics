f2920c620f7eac8960181ccbfa6e2ee147301136
harshadalohar123 <harshalohar123@gmail.com>
2022-03-22 23:27:21 +0530

    ADDED:Find the Max value Using Generic Method

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactor_1
+{
+    public class Generic<T> where T : IComparable
+    {
+        public T MaxValue(T First_Value, T Second_Value, T Third_Value)
+        {
+            if (First_Value.CompareTo(Second_Value) > 0)
+            {
+                if (First_Value.CompareTo(Third_Value) > 0)
+                {
+                    return First_Value;
+                }
+                else
+                {
+                    return Third_Value;
+                }
+            }
+            else
+            {
+                if (Second_Value.CompareTo(Third_Value) > 0)
+                {
+                    return Second_Value;
+                }
+                else
+                {
+                    return Third_Value;
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Refactor_1
+{
+    public class Generic
+    {
+        static void Main(string[] args)
+        {
+            Generic<int> obj = new Generic<int>();
+            int result =obj.MaxValue(104, 90, 160);
+            Console.WriteLine("Max Value Of Integer Is : "+result);
+
+            Generic<double> obj1 = new Generic<double>();
+            double result1 = obj1.MaxValue(23.3,34.6,556.5);
+            Console.WriteLine("max value of double is : " + result1);
+
+            Generic<string> obj2 = new Generic<string>();
+            string result2 = obj2.MaxValue("Apple","Peach","Banana");
+            Console.WriteLine("Max Value Of String Is : " + result2);
+
+        }
+    }
+}
