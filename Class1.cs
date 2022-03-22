0381947fac30da11ddf7c9ced1038e384278dbb2
harshadalohar123 <harshalohar123@gmail.com>
2022-03-22 23:22:22 +0530

    ADDED:Max Method Using Generic

---
+++ Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Generic_Uc5
+{
+    internal class Uc5_Generic<T> where T : IComparable
+    {
+        public T[] value;
+
+        public Uc5_Generic(T[] value)
+        {
+            this.value = value;
+        }
+
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+
+        public T MaxValue(T[] values)
+        {
+            T[] SortedValues = Sort(values);
+            return SortedValues[SortedValues.Length-1];
+        }
+
+        public T MaxValue()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("The maximum is: " + max);
+        }
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Generic_Uc5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Length of Array");
+            int len = int.Parse(Console.ReadLine());
+            int[] arr = new int[len];
+            Console.WriteLine("Enter elements of array");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Uc5_Generic<int> gen = new Uc5_Generic<int>(arr);
+            gen.PrintMaxValue();
+        }
+    }
+}
